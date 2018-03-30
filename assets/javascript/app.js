057e69c5cf36c7a5c692c9970a5c805bc1d2384d
Myeager86 <31748384+Myeager86@users.noreply.github.com>
2018-03-29 20:51:44 -0600

    maps

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -112,54 +112,43 @@ $(document).ready(function () {
     // });
 
 
-    function initialize() {
-        // Create a map to show the results, and an info window to
-        // pop up if the user clicks on the place marker.
-        var pyrmont = new google.maps.LatLng(39.7392, -104.9903);
-      
-        var map = new google.maps.Map(document.getElementById('map'), {
-          center: pyrmont,
-          zoom: 15,
-          scrollwheel: false
-        });
-        var infowindow = new google.maps.InfoWindow();
-        var service = new google.maps.places.PlacesService(map);
-      
-        document.getElementById('submit').addEventListener('click', function() {
-          placeDetailsByPlaceId(service, map, infowindow);
-        });
+    var map;
+
+function initialize() {
+  // Create a map centered in Pyrmont, Sydney (Australia).
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 39.7392, lng: -104.9903},
+    zoom: 15
+  });
+
+  // Search for Google's office in Australia.
+  var request = {
+    location: map.getCenter(),
+    radius: '500',
+    query: 'Google Sydney'
+  };
+
+  var service = new google.maps.places.PlacesService(map);
+  service.textSearch(request, callback);
+}
+
+// Checks that the PlacesServiceStatus is OK, and adds a marker
+// using the place ID and location from the PlacesService.
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    var marker = new google.maps.Marker({
+      map: map,
+      place: {
+        placeId: results[0].place_id,
+        location: results[0].geometry.location
       }
-      
-      function placeDetailsByPlaceId(service, map, infowindow) {
-        // Create and send the request to obtain details for a specific place,
-        // using its Place ID.
-        var request = {
-          placeId: document.getElementById('place-id').value
-        };
-      
-        service.getDetails(request, function (place, status) {
-          if (status == google.maps.places.PlacesServiceStatus.OK) {
-            // If the request succeeds, draw the place location on the map
-            // as a marker, and register an event to handle a click on the marker.
-            var marker = new google.maps.Marker({
-              map: map,
-              position: place.geometry.location
-            });
-      
-            google.maps.event.addListener(marker, 'click', function() {
-              infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
-                'Place ID: ' + place.place_id + '<br>' +
-                place.formatted_address + '</div>');
-              infowindow.open(map, this);
-            });
-      
-            map.panTo(place.geometry.location);
-          }
-        });
-      }
-      
-      // Run the initialize function when the window has finished loading.
-      google.maps.event.addDomListener(window, 'load', initialize);
+    });
+  }
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+
     var weatherAPIKey = "5dd2a3eaffbc2b464e3e24b8a9a82bb7";
 
     // Here we are building the URL we need to query the database
