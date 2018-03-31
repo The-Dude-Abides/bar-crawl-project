1d8f74b2a81746e89ffacbc87df55fe11b667596
Myeager86 <31748384+Myeager86@users.noreply.github.com>
2018-03-31 11:45:44 -0600

    fixed undefined

--- assets/javascript/welcome.js
+++ assets/javascript/welcome.js
@@ -40,20 +40,38 @@ $(document).ready(function () {
             document.getElementById("logInJumbotron").hide;
         }
     });
+    
+    function signUp() {
+
+        const userEmailSignUp = $("#signUpEmail").val();
+        const userPassSignUp = $("#signUpPassword").val();
+
+        console.log(userEmailSignUp + ' ' + userPassSignUp)
+
+        firebase.auth().createUserWithEmailAndPassword(userEmailSignUp, userPassSignUp).catch(function (error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+
+            window.alert("error: " + errorMessage);
+            // ...
+        });
+
+    }
 
     $('#signUpSubmitButton').on('click', function (event) {
         event.preventDefault();
         signUp();
     });
 
-    function signUp() {
 
-        var userEmailSignUp = $("#signUpEmail").val();
-        var userPassSignUp = $("#signUpPassword").val();
+    function login() {
+        var userEmail = $(".logInEmail").val();
+        var userPass = $(".logInPass").val();
 
-        console.log(userEmailSignUp + ' ' + userPassSignUp)
+        console.log(userEmail + ' ' + userPass)
 
-        firebase.auth().createUserWithEmailAndPassword(userEmailSignUp, userPassSignUp).catch(function (error) {
+        firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function (error) {
             // Handle Errors here.
             var errorCode = error.code;
             var errorMessage = error.message;
@@ -69,98 +87,14 @@ $(document).ready(function () {
         login();
     });
 
-    function login() {
-        var userEmail = $("#logInEmail").val();
-        var userPass = $("#logInPass").val();
-
-        console.log(userEmail + ' ' + userPass)
-
-        firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function (error) {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-
-            window.alert("error: " + errorMessage);
-            // ...
-        });
-
-    }
-
-    function logout() {
-        firebase.auth().signOut();
-        console.log('logged out');
-    }
-
     $('#logOutButton').on('click', function (event) {
         event.preventDefault();
         logout();
     });
 
-
-
-
-    // // get elements
-    // const txtEmail = document.getElementById('signUpEmail');
-    // const txtPassword = document.getElementById('signUpPassword');
-    // const signUpButton = document.getElementById('signUpSubmitButton');
-    // const logInButton = document.getElementById('logInButton');
-    // const logOutButton = document.getElementById('logOut');
-
-    // // add login event
-    // logInButton.addEventListener('click', e => {
-    //     // get email and pass
-    //     const email = txtEmail.val().trim();
-    //     const password = txtPassword.val().trim();
-    //     const auth = firebase.auth();
-
-    //     // sign in
-    //     const promise = auth.signInWithEmailAndPassword(email, pass);
-    //     promise.catch(e => console.log(e.message));
-    // });
-
-    // // add signup event
-    // signUpButton.addEventListener('click', e => {
-    //     // get email and pass
-    //     const email = txtEmail.val().trim();
-    //     const password = txtPassword.val().trim();
-    //     const auth = firebase.auth();
-
-    //     // sign in
-    //     const promise = auth.createUserWithEmailAndPassword(email, pass);
-    //     promise.catch(e => console.log(e.message));
-    // });
-
-    // // add realtime listener
-    // firebase.auth().onAuthStateChanged(firebaseUser => {
-    //     if (firebaseUser) {
-    //         console.log(firebaseUser);
-    //     } else {
-    //         console.log('not logged in');
-    //     };
-    // });
-
-
-
-    // var database = firebase.database();
-
-    // // initial variables
-    // var email = '';
-    // var password = '';
-
-    // $('#signUpSubmitButton').on('click', function (event) {
-    //     //prevent page from refreshing
-    //     event.preventDefault();
-
-    //     // changing variables to what is inputted by user
-    //     email = $('#email').val().trim();
-    //     password = $('#password').val().trim();
-
-    //     database.ref().push({
-    //         email: email,
-    //         password: password
-    //     });
-
-    //     email = $('#email').val("");
-    //     password = $('#password').val("");
+    function logout() {
+        firebase.auth().signOut();
+        console.log('logged out');
+    }
 
 });
